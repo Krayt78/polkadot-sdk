2a610482b8c71e6ce5226d862d8963515ec8804a
github-action-benchmark <github@users.noreply.github.com>
2024-11-19 16:02:47 +0000

    add availability-distribution-regression-bench (customSmallerIsBetter) benchmark result for 8d4138f77106a6af49920ad84f3283f696f3f905

--- bench/availability-distribution-regression-bench/data.js
+++ bench/availability-distribution-regression-bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732018267858,
+  "lastUpdate": 1732032167259,
   "repoUrl": "https://github.com/paritytech/polkadot-sdk",
   "entries": {
     "availability-distribution-regression-bench": [
@@ -37663,6 +37663,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "availability-store",
             "value": 0.17913716387999987,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "maciej.zyszkiewicz@parity.io",
+            "name": "Maciej",
+            "username": "Overkillus"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d4138f77106a6af49920ad84f3283f696f3f905",
+          "message": "Validator Re-Enabling (#5724)\n\nAims to implement Stage 3 of Validator Disbling as outlined here:\nhttps://github.com/paritytech/polkadot-sdk/issues/4359\n\nFeatures:\n- [x] New Disabling Strategy (Staking level)\n- [x] Re-enabling logic (Session level)\n- [x] More generic disabling decision output\n- [x] New Disabling Events\n\nTesting & Security:\n- [x] Unit tests\n- [x] Mock tests\n- [x] Try-runtime checks\n- [x] Try-runtime tested on westend snap\n- [x] Try-runtime CI tests\n- [ ] Re-enabling Zombienet Test (?)\n- [ ] SRLabs Audit\n\nCloses #4745 \nCloses #2418\n\n---------\n\nCo-authored-by: ordian <write@reusable.software>\nCo-authored-by: Ankan <10196091+Ank4n@users.noreply.github.com>\nCo-authored-by: Tsvetomir Dimitrov <tsvetomir@parity.io>",
+          "timestamp": "2024-11-19T14:40:25Z",
+          "tree_id": "83f2cf93ba1c0f793e28828d628861fa4a3d33f6",
+          "url": "https://github.com/paritytech/polkadot-sdk/commit/8d4138f77106a6af49920ad84f3283f696f3f905"
+        },
+        "date": 1732032148195,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Sent to peers",
+            "value": 18481.666666666653,
+            "unit": "KiB"
+          },
+          {
+            "name": "Received from peers",
+            "value": 433.3333333333332,
+            "unit": "KiB"
+          },
+          {
+            "name": "availability-store",
+            "value": 0.17863561733999997,
+            "unit": "seconds"
+          },
+          {
+            "name": "availability-distribution",
+            "value": 0.015985874573333336,
+            "unit": "seconds"
+          },
+          {
+            "name": "test-environment",
+            "value": 0.008803903420000097,
+            "unit": "seconds"
+          },
+          {
+            "name": "bitfield-distribution",
+            "value": 0.02469214513333333,
             "unit": "seconds"
           }
         ]
